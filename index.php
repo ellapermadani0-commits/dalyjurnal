d80ef6f0720909330ea48536b73efc47efb015a4
ella nuraini <ellapermadani0@gmail.com>
2026-01-10 18:23:25 +0700

    ubah judul web

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include "koneksi.php";
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Comfortcorner</title>
+    <title>My Daily Journal</title>
     <link rel="icon" type="image/png" href="img/logo.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
